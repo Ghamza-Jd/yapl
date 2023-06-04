19fec5b2679898020827213afeff520e1d7dbfd6
Ghamza-Jd <haljadid@proximie.com>
2023-06-04 23:58:18 +0300

    feat: added line info

--- chunk.h
+++ chunk.h
@@ -13,12 +13,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
     value_array_t constants;
 } chunk_t;
 
 void init_chunk(chunk_t* chunk);
 void free_chunk(chunk_t* chunk);
-void write_chunk(chunk_t* chunk, uint8_t byte);
+void write_chunk(chunk_t* chunk, uint8_t byte, int line);
 int add_constant(chunk_t* chunk, value_t value);
 
 #endif
