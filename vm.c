3725205b60eadaf2a5fecbd0e12f496417daebfc
Ghamza-Jd <haljadid@proximie.com>
2023-06-05 00:23:27 +0300

    feat: interpret bytecode for vm

--- vm.c
+++ vm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -7,12 +9,20 @@ void init_vm() { }
 
 void free_vm() { }
 
-static InterpretResult run() {
+static interpret_result_t run() {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        value_t constant = READ_CONSTANT();
+        print_value(constant);
+        printf("\n");
+        break;
+      }
+
       case OP_RETURN: {
         return INTERPRET_OK;
       }
@@ -20,6 +30,7 @@ static InterpretResult run() {
   }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 interpret_result_t interpret(chunk_t* chunk) {
