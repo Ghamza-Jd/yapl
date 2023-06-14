12f5a6c06efc5fcd4076e9a423376a3210c05a20
Hamza Jadid <haljadid@proximie.com>
2023-06-14 08:53:54 +0300

    feat: added bool and number types

--- value.h
+++ value.h
@@ -17,18 +17,24 @@ typedef struct value_t {
     } as;
 } value_t;
 
-#define AS_BOOL(value) ((value).as.boolean)
-#define AS_NUMBER(value) ((value).as.number)
+#define IS_BOOL(value)      ((value).type == VAL_BOOL)
+#define IS_NIL(value)       ((value).type == VAL_NIL)
+#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
 
-#define BOOL_VAL(value) ((value_t){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL ((value_t){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((value_t){VAL_NUMBER, {.number = value}})
+#define AS_BOOL(value)      ((value).as.boolean)
+#define AS_NUMBER(value)    ((value).as.number)
+
+#define BOOL_VAL(value)     ((value_t){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL             ((value_t){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value)   ((value_t){VAL_NUMBER, {.number = value}})
 
 typedef struct value_array_t {
     int capacity;
     int count;
     value_t *values;
 } value_array_t;
+
+bool values_equal(value_t a, value_t b);
 
 void init_value_array(value_array_t *array);
 
